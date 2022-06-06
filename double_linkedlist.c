89838fc712fcc5393aabebeecfb1c2957172e5fe
sireesha <97424637+21A91A05F2@users.noreply.github.com>
2022-06-06 12:42:26 +0530

    Add files via upload

---
+++ double_linkedlist.c
@@ -0,0 +1,206 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+	struct node *prev;
+	int data;
+	struct node *next;
+};
+typedef struct node node;
+node *head=NULL;
+void insert_at_end(int val)
+{
+	node *nn,*temp;
+	nn=(node *)malloc(sizeof(node));
+	nn->prev=NULL;
+	nn->data=val;
+	nn->next=NULL;
+	if(head==NULL)
+	{
+		head=nn;
+	}
+	else
+	{
+		temp=head;
+		while(temp->next!=NULL)
+		{
+			temp=temp->next;
+		}
+		temp->next=nn;
+		nn->prev=temp;
+	}
+}
+int delete_at_end()
+{
+	int val;
+	node *temp,*temp1;
+	if(head==NULL)
+	{
+		return -1;
+	}
+	else if(head->next==NULL)
+	{
+		val=head->data;
+		head=NULL;
+		return val;
+	}
+	else
+	{
+		temp=head;
+		while(temp->next->next!=NULL)
+		{
+			temp=temp->next;
+		}
+		temp1=temp->next;//taking temp1 to last node
+		temp->next=NULL;
+		temp1->prev=NULL;
+		return temp1->data;
+	}
+}
+void display()
+{
+	node *temp=head;
+	if(temp==NULL)
+	{
+		printf("NO NODES\n");
+	}
+	else
+	{
+		while(temp!=NULL)
+		{
+			printf("(%d) %d %d %d\n",temp,temp->prev,temp->data,temp->next);//temp is present node's address
+			temp=temp->next;
+		}
+	}
+}
+void insert_at_head(int val)
+{
+	node *nn;
+	nn=(node *)malloc(sizeof(node));
+	nn->prev=NULL;
+	nn->data=val;
+	nn->next=NULL;
+	if(head==NULL)
+	{
+		head=nn;
+	}
+	else
+	{
+		nn->next=head;
+		head->prev=nn;
+		head=nn;
+	}
+}
+int delete_at_head()
+{
+	int val;
+	node *temp;
+	if(head==NULL)
+	{
+		return -1;
+	}
+	else if(head->next==NULL)
+	{
+		val=head->data;
+		head=NULL;
+		return val;
+	}
+	else
+	{
+		temp=head->next;
+		val=head->data;
+		temp->prev=NULL;
+		head->next=NULL;
+		head=temp;
+		return val;
+	}
+}
+int main()
+{
+	int ch,val,pos;
+	while(1)
+	{
+		printf("1)Insert at end 2)Delete at end 3)Display 4)Insert at head 5)Delete at head 6)Exit\n");
+		scanf("%d",&ch);
+		if(ch==1)
+		{
+			scanf("%d",&val);
+			insert_at_end(val);
+		}
+		else if(ch==2)
+		{
+			val=delete_at_end();
+			if(val==-1)
+			{
+				printf("NO NODES TO DELETE\n");
+			}
+			else
+			{
+				printf("%d\n",val);
+			}
+		}
+		else if(ch==3)
+		{
+			display();
+		}
+		else if(ch==4)
+		{
+			scanf("%d",&val);
+			insert_at_head(val);
+		}
+		else if(ch==5)
+		{
+			val=delete_at_head();
+			if(val==-1)
+			{
+				printf("NO NODES TO DELETE\n");
+			}
+			else
+			{
+				printf("%d\n",val);
+			}
+		}
+		else
+		{
+			break;
+		}
+	}
+}
+/*1)Insert at end 2)Delete at end 3)Display 4)Insert at head 5)Delete at head 6)Exit
+1
+23
+1)Insert at end 2)Delete at end 3)Display 4)Insert at head 5)Delete at head 6)Exit
+1
+34
+1)Insert at end 2)Delete at end 3)Display 4)Insert at head 5)Delete at head 6)Exit
+1
+45
+1)Insert at end 2)Delete at end 3)Display 4)Insert at head 5)Delete at head 6)Exit
+5
+23
+1)Insert at end 2)Delete at end 3)Display 4)Insert at head 5)Delete at head 6)Exit
+3
+(7017520) 0 34 7017552
+(7017552) 7017520 45 0
+1)Insert at end 2)Delete at end 3)Display 4)Insert at head 5)Delete at head 6)Exit
+4
+5
+1)Insert at end 2)Delete at end 3)Display 4)Insert at head 5)Delete at head 6)Exit
+4
+67
+1)Insert at end 2)Delete at end 3)Display 4)Insert at head 5)Delete at head 6)Exit
+5
+67
+1)Insert at end 2)Delete at end 3)Display 4)Insert at head 5)Delete at head 6)Exit
+4
+12
+1)Insert at end 2)Delete at end 3)Display 4)Insert at head 5)Delete at head 6)Exit
+3
+(7017648) 0 12 7017584
+(7017584) 7017648 5 7017520
+(7017520) 7017584 34 7017552
+(7017552) 7017520 45 0
+1)Insert at end 2)Delete at end 3)Display 4)Insert at head 5)Delete at head 6)Exit
+6
+
+--------------------------------*/
